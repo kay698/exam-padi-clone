9d9f505c312c89a7a69135690e995f63f157121d
keji <tomitoyo.93@gmail.com>
2021-07-27 10:20:01 +0100

    commit

--- src/routes/Dashboard/CBTPage/NecoSat/sat.jsx
+++ src/routes/Dashboard/CBTPage/NecoSat/sat.jsx
@@ -2,50 +2,49 @@ import { useState, useEffect, useContext } from "react";
 import CbtLayout from "../../../../components/CBTlayout";
 import { NecoSatPageStyles } from "./styles";
 import { FlexibleDiv } from "../../../../components/Box/styles";
-import { SAT } from "../../../../utils/dataHelpers/NecoSat";
+import { Neco } from "../../../../utils/dataHelpers/NecoSat";
 import { Select } from "antd";
 import { ModeContext } from "../../../../context/Mode";
 import QandA from "../../../../components/QandA";
 import { Button, Modal } from "antd";
 import QandATest from "../../../../components/QandA/test";
-import 'antd/dist/antd.css';
+import "antd/dist/antd.css";
+import { Paginate } from "../../../../components/QandA/pagination";
 
-
-
-const Sat = () => {
+const NECO = () => {
 	const [isModalVisible, setIsModalVisible] = useState(false);
 	const { Option } = Select;
 	const [modeContext] = useContext(ModeContext);
-	const subjects = [...new Set(SAT.map((item) => item.subject))];
+	const subjects = [...new Set(Neco.map((item) => item.subject))];
 	const [selectedSubject, setSelectedSubject] = useState("Physics");
 	const [selectedYear, setSelectedYear] = useState("2021");
 	const [data, setData] = useState();
+	const [score, setScore] = useState(0);
 
 	const filterYear = (year) => {
 		setSelectedYear(year);
-		const filteredYearResults = SAT.filter(
+		const filteredYearResults = Neco.filter(
 			(el) => year === el.examYear && selectedSubject === el.subject
 		);
 		setData(filteredYearResults[0]);
 	};
 	const filterSubject = (subject) => {
 		setSelectedSubject(subject);
-		const filteredSubjectResults = SAT.filter((el) => subject === el.subject);
+		const filteredSubjectResults = Neco.filter((el) => subject === el.subject);
 		setData(filteredSubjectResults[0]);
 	};
 
 	const showModal = () => {
 		setIsModalVisible(true);
-};
+	};
 
-const handleOk = () => {
+	const handleOk = () => {
 		setIsModalVisible(false);
-};
+	};
 
-const handleCancel = () => {
+	const handleCancel = () => {
 		setIsModalVisible(false);
-};
-
+	};
 
 	useEffect(() => {
 		console.log({ modeContext });
@@ -53,9 +52,9 @@ const handleCancel = () => {
 	}, [modeContext]);
 
 	const onIntialLoad = () => {
-		setSelectedYear(SAT[0].examYear);
+		setSelectedYear(Neco[0].examYear);
 		setSelectedSubject(subjects[0]);
-		const filteredSubjectResults = SAT.filter(
+		const filteredSubjectResults = Neco.filter(
 			(el) => subjects[0] === el.subject
 		);
 		setData(filteredSubjectResults[0]);
@@ -70,7 +69,7 @@ const handleCancel = () => {
 							defaultValue={selectedSubject}
 							onChange={filterSubject}
 							style={{ width: 120 }}
-							bordered ={false}>
+							bordered={false}>
 							<Option defaultValue="default" disabled>
 								Select Subject
 							</Option>
@@ -85,12 +84,12 @@ const handleCancel = () => {
 							defaultValue={selectedYear}
 							onChange={filterYear}
 							style={{ width: 120 }}
-							bordered ={false}
+							bordered={false}
 							className="selec">
 							<Option defaultValue="default" disabled>
 								Select Year
 							</Option>
-							{SAT.map((item, i) => (
+							{Neco.map((item, i) => (
 								<Option key={i} value={item.examYear}>
 									{item.examYear}
 								</Option>
@@ -106,29 +105,52 @@ const handleCancel = () => {
 							data.data.map((el, index) => <QandA item={el} key={index} />)}
 						{modeContext === "test" &&
 							data &&
-							data.data.map((el, index) => <QandATest item={el} key={index} />)}
+							data.data.map((el, index) => (
+								<QandATest
+									item={el}
+									key={index}
+									score={score}
+									setScore={setScore}
+								/>
+							))}
 					</FlexibleDiv>
 				</>
 
 				<FlexibleDiv justifyContent="center">
-					{modeContext === "test" && <Button className="buts" onClick={()=> showModal(true)}>Submit</Button>}
+					{modeContext === "test" && (
+						<Button className="buts" onClick={() => showModal(true)}>
+							Submit
+						</Button>
+					)}
 				</FlexibleDiv>
 
 				<>
-     {modeContext === "test" &&  <Modal  visible={isModalVisible} onOk={handleOk} onCancel={handleCancel} footer={null} >
-       
-							<p>Some contents...</p>
-							<p>Some contents...</p>
-							<p>Some contents...</p>
+					{modeContext === "test" && (
+						<Modal
+							visible={isModalVisible}
+							onOk={handleOk}
+							onCancel={handleCancel}
+							footer={null} 	bodyStyle={{ fontSize: "30px"}}
+							style={{borderRadius: "15px"}}
+							centered>
+						
 							
-					</Modal>}
-    </>
-        
+								<h3 className="h1">Congratulations</h3>
+								<p className="para"> Your test score is</p>
+								<p className="para">
+									{score}/{data.data.length}
+								</p>
+						
+						</Modal>
+					)}
+				</>
+
 				<FlexibleDiv justifyContent="flex-end">
+					<Paginate />
 				</FlexibleDiv>
 			</NecoSatPageStyles>
 		</CbtLayout>
 	);
 };
 
-export default Sat;
+export default NECO;
